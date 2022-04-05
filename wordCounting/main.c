86cfdfd12e7f93f230fdd700dda9b8f36f4db5df
dgoceva <44052125+dgoceva@users.noreply.github.com>
2022-04-05 11:12:56 +0300

    wordCounting added

---
+++ wordCounting/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BOOLEAN int
+#define FALSE 0
+
+int main()
+{
+    char c;
+    int nc,nw,nl;
+    BOOLEAN isInside=FALSE;
+
+    nc=nw=nl=0;
+    while((c=getchar())!=EOF){ // ^Z===EOF
+        nc++;
+        if (c=='\n'){
+            ++nl;
+        }
+        if (c=='\n' || c==' ' || c=='\t'){
+            isInside = FALSE;
+        }
+        else if(!isInside){
+            isInside = !isInside;
+            nw++;
+        }
+
+    }
+    printf("Chars: %d\tWords: %d\tLines: %d\n",nc,nw,nl);
+
+    return 0;
+}
